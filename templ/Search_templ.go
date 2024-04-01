19d2d60041088914b569e811368dbf8b9b49e310
tris203 <admin@snappeh.com>
2024-04-01 23:47:00 +0100

    fix: search layout

--- templ/Search_templ.go
+++ templ/Search_templ.go
@@ -198,11 +198,11 @@ func SearchForm() templ.Component {
 			templ_7745c5c3_Var10 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 12)
+		templ_7745c5c3_Err = TopBar("htmxx", "Search").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = TopBar("htmxx", "Search").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = templ.WriteWatchModeString(templ_7745c5c3_Buffer, 12)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
