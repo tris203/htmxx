7450555cdcf38b40db9708b950cbd95b05b72577
tris203 <admin@snappeh.com>
2024-04-02 15:17:30 +0100

    fix: typo

--- templ/Tweet_templ.go
+++ templ/Tweet_templ.go
@@ -145,9 +145,9 @@ func likes(tweet *model.Tweet) templ.Component {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var9 string
-			templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(getIDURL(" tweet", tweet.ID))
+			templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(getIDURL("tweet", tweet.ID))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templ/Tweet.templ`, Line: 76, Col: 44}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templ/Tweet.templ`, Line: 76, Col: 43}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 			if templ_7745c5c3_Err != nil {
