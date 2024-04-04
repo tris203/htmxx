28b181b33ef34f73cdee13f677cd67b4ac9eda64
tris203 <admin@snappeh.com>
2024-04-04 16:28:03 +0100

    fix: url string

--- service/db_service.go
+++ service/db_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"database/sql"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 	"htmxx/db"
@@ -17,7 +18,7 @@ func (s *DBService) Connect() (context.Context, *db.Queries, *sql.DB, error) {
 	if url == "" {
 		panic("HTMXX_DB_URL must be set")
 	}
-	dbConn, err := sql.Open("libsql", url)
+	dbConn, err := sql.Open("libsql", fmt.Sprintf("libsql://%s", url))
 	if err != nil {
 		return nil, nil, nil, err
 	}
